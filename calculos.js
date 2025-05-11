7d5a2b2638f12396c89fe59a8689e5629bd86fa4
Carlos5456 <carloscesar.araldi@gmail.com>
2025-05-11 12:46:39 -0300

    2

--- calculos.js
+++ calculos.js
@@ -23,60 +23,60 @@ function proximoDiaUtil(dia, ignorarDomingos) {
 }
 
 function calcularViagens(entrada) {
-  return entrada.map(viagem => {
-    const {
-      data_saida,
-      horario_inicio,
-      horario_fim,
-      duracao_almoco,
-      ignorar_domingos
-    } = viagem.dadosSaida;
+  const { dadosSaida, trechos } = entrada.body; // Acessa o objeto "body" dentro da entrada
 
-    const almocoMin = parseInt(duracao_almoco.split(':')[0], 10); // vem como "30:00"
-    const expedienteInicio = horario_inicio;
-    const expedienteFim = horario_fim;
+  const {
+    data_saida,
+    horario_inicio,
+    horario_fim,
+    duracao_almoco,
+    ignorar_domingos
+  } = dadosSaida;
 
-    let dataAtual = dayjs(`${data_saida} ${expedienteInicio}`);
-    let fimDia = dayjs(`${data_saida} ${expedienteFim}`);
-    let almocoFeitoHoje = false;
+  const almocoMin = parseInt(duracao_almoco.split(':')[0], 10); // vem como "30:00"
+  const expedienteInicio = horario_inicio;
+  const expedienteFim = horario_fim;
 
-    const trechosCalculados = [];
+  let dataAtual = dayjs(`${data_saida} ${expedienteInicio}`);
+  let fimDia = dayjs(`${data_saida} ${expedienteFim}`);
+  let almocoFeitoHoje = false;
 
-    for (const trecho of viagem.trechos) {
-      let duracaoMinutos = minutosDuracao(trecho.duracao);
-      let inicioTrecho = dataAtual;
+  const trechosCalculados = [];
 
-      while (duracaoMinutos > 0) {
-        fimDia = dayjs(`${dataAtual.format('YYYY-MM-DD')} ${expedienteFim}`);
-        let minutosRestantesHoje = fimDia.diff(dataAtual, 'minute');
+  for (const trecho of trechos) {
+    let duracaoMinutos = minutosDuracao(trecho.duracao);
+    let inicioTrecho = dataAtual;
 
-        if (!almocoFeitoHoje && dataAtual.format('HH:mm') < '12:00' && fimDia.format('HH:mm') > '12:00') {
-          minutosRestantesHoje -= almocoMin;
-          almocoFeitoHoje = true;
-        }
+    while (duracaoMinutos > 0) {
+      fimDia = dayjs(`${dataAtual.format('YYYY-MM-DD')} ${expedienteFim}`);
+      let minutosRestantesHoje = fimDia.diff(dataAtual, 'minute');
 
-        if (duracaoMinutos <= minutosRestantesHoje) {
-          dataAtual = adicionarMinutos(dataAtual, duracaoMinutos);
-          break;
-        } else {
-          duracaoMinutos -= minutosRestantesHoje;
-          dataAtual = proximoDiaUtil(dataAtual, ignorar_domingos).hour(Number(expedienteInicio.split(':')[0])).minute(Number(expedienteInicio.split(':')[1]));
-          almocoFeitoHoje = false;
-        }
+      if (!almocoFeitoHoje && dataAtual.format('HH:mm') < '12:00' && fimDia.format('HH:mm') > '12:00') {
+        minutosRestantesHoje -= almocoMin;
+        almocoFeitoHoje = true;
       }
 
-      trechosCalculados.push({
-        origem: trecho.origem,
-        destino: trecho.destino,
-        SAIDA: formatar(inicioTrecho),
-        CHEGADA: formatar(dataAtual),
-        distancia: trecho.distancia,
-        duracao: trecho.duracao
-      });
+      if (duracaoMinutos <= minutosRestantesHoje) {
+        dataAtual = adicionarMinutos(dataAtual, duracaoMinutos);
+        break;
+      } else {
+        duracaoMinutos -= minutosRestantesHoje;
+        dataAtual = proximoDiaUtil(dataAtual, ignorar_domingos).hour(Number(expedienteInicio.split(':')[0])).minute(Number(expedienteInicio.split(':')[1]));
+        almocoFeitoHoje = false;
+      }
     }
 
-    return trechosCalculados;
-  }).flat();
+    trechosCalculados.push({
+      origem: trecho.origem,
+      destino: trecho.destino,
+      SAIDA: formatar(inicioTrecho),
+      CHEGADA: formatar(dataAtual),
+      distancia: trecho.distancia,
+      duracao: trecho.duracao
+    });
+  }
+
+  return trechosCalculados;
 }
 
 module.exports = { calcularViagens };
