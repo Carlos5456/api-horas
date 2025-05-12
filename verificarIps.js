db1b081ebc6307812f2112f774bc130dfc4ba3ad
Carlos5456 <carloscesar.araldi@gmail.com>
2025-05-12 18:12:44 -0300

    ipteste

--- verificarIps.js
+++ verificarIps.js
@@ -1,5 +1,5 @@
 // middleware/verificarIps.js
-const ipsPermitidos = ['::ffff:10.11.0.4']; // Substitua pelos IPs permitidos
+const ipsPermitidos = ['xxx.xxx.xxx.xx']; // Substitua pelos IPs permitidos
 
 function verificarIps(req, res, next) {
   const ipCliente = req.ip;
