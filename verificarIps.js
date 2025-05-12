6ec16b53a91d48b347a3236af7fe16694acf9355
Carlos5456 <carloscesar.araldi@gmail.com>
2025-05-12 18:07:29 -0300

    Ip vm

--- verificarIps.js
+++ verificarIps.js
@@ -1,5 +1,5 @@
 // middleware/verificarIps.js
-const ipsPermitidos = ['62.72.63.240']; // Substitua pelos IPs permitidos
+const ipsPermitidos = ['::ffff:10.11.0.4']; // Substitua pelos IPs permitidos
 
 function verificarIps(req, res, next) {
   const ipCliente = req.ip;
